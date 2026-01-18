0101bce314955b00a5bfd9d756761b8ffc11fa14
ParsyPage <drsparssy28e7@gmail.com>
2026-01-18 16:26:57 +0530

    First js program

---
+++ javascript/first.js
@@ -0,0 +1,9 @@
+const frnds_age = [20,21,20,19,30]
+const frnds = 5
+const total_age = frnds_age[0]+frnds_age[1]+frnds_age[2]+frnds_age[3]+frnds_age[4]
+
+console.log("Sum of ages of all frnds is ", total_age)
+
+const avg_age = total_age/frnds
+
+console.log("Avg_age og frnds is ", avg_age)
